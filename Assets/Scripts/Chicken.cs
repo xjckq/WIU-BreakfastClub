bc78f4a1d68c08a2ff71e7bb1561f1623f379653
13-Josline <jckqxx@gmail.com>
2025-08-21 06:23:06 +0800

    test

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -3,23 +3,86 @@ using UnityEngine;
 public class Chicken : MonoBehaviour
 {
     public float speed = 2;
-    public float stopDistance = 1;
-    [SerializeField] Transform player;
-    Vector2 playerPos, chickenPos, newPos;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float stopDistance = 1.5f;
+    Vector2 playerPos, chickenPos, newPos, playerLastPos;
+
+    [SerializeField] GameObject player;
+
+    public float pushDetectionRadius = 1.5f;
+    public float pushDistance = 1.2f;
+    public float pushSpeed = 4;
+    public float pushDuration = .8f;
+    public float pushRandomness = .3f;
+
+    float pushTimer = 0;
+    Vector2 pushTargetPos;
+    float distanceToPlayer;
+
+    Rigidbody2D playerRB;
+
     void Start()
     {
-        
+        playerRB = player.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         chickenPos = transform.position;
-        playerPos = player.position;
+        playerPos = player.transform.position;
 
-        newPos = Vector2.MoveTowards(chickenPos,playerPos,speed * Time.deltaTime);
+        pushTimer -= Time.deltaTime;
 
-        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+        if (pushTimer > 0f)
+        {
+            // move toward push target
+            newPos = Vector2.MoveTowards(chickenPos, pushTargetPos, pushSpeed * Time.deltaTime);
+            transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+            return;
+        }
+
+
+        distanceToPlayer = Vector2.Distance(chickenPos, playerPos);
+
+        // check for push behavior
+        if (distanceToPlayer < pushDetectionRadius && IsPlayerMovingTowardsChicken())
+        {
+            Vector2 pushDirection = (chickenPos - playerPos).normalized;
+
+            // set push target position
+            pushTargetPos = chickenPos + pushDirection * pushDistance;
+            pushTimer = pushDuration;
+
+            // face away from player
+            if (pushDirection.x > 0)
+                transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+            else
+                transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+
+            return;
+        }
+
+        // follow player
+        if (distanceToPlayer > stopDistance)
+        {
+            newPos = Vector2.MoveTowards(chickenPos, playerPos, speed * Time.deltaTime);
+            transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+        }
     }
+
+    bool IsPlayerMovingTowardsChicken()
+    {
+        // check if player is moving fast enough
+        if (playerRB.linearVelocity.magnitude < 0.5f)
+            return false;
+
+        // check if player is getting closer to chicken
+        float lastDistance = Vector2.Distance(playerLastPos, chickenPos);
+        float currentDistance = Vector2.Distance(playerPos, chickenPos);
+
+        playerLastPos = playerPos; 
+
+        return currentDistance < lastDistance;
+    }
+
 }
+
