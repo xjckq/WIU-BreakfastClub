6f51f62322c9a5de6cc2a4fef6ba6aadf4e486fc
ironmanking <braxtonhong2000@gmail.com>
2025-08-20 12:06:37 +0800

    enemy

---
+++ Assets/Scripts/CheckTrigger.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CollisionTarget
+{
+    public Collider2D collider;
+    public UnityEvent onCollisionEnter;
+    public UnityEvent onCollisionExit;
+}
+
+public class CheckTrigger : MonoBehaviour
+{
+    public CollisionTarget[] targets;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach (CollisionTarget target in targets)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                Debug.Log("Touched: " + target.collider.name);
+                target.onCollisionEnter.Invoke();
+                return;
+            }
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        foreach (CollisionTarget target in targets)
+        {
+            if (other == target.collider)
+            {
+                target.onCollisionExit.Invoke();
+                return;
+            }
+        }
+    }
+}
