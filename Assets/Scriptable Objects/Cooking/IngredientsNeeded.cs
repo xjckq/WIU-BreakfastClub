bc794054ca9138c018ff2258ecc4f33bfa72e07a
13-Josline <jckqxx@gmail.com>
2025-08-22 02:18:52 +0800

    Revert "Merge pull request #13 from xjckq/Ashyln"
    This reverts commit da1ec734a603e1bbe1bd765d82658d043023503a, reversing
    changes made to b2a014714604dc9e01312a2246229de22c4cf5dc.

--- Assets/Scriptable Objects/Cooking/IngredientsNeeded.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "IngredientsNeeded", menuName = "Scriptable Objects/IngredientsNeeded")]
-public class IngredientsNeeded : ScriptableObject
-{
-    public ItemData ingredient;
-    public int amount;
-}
